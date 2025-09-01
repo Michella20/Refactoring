4aabd3aaa580a7cf113aa4fb2b6693f41b5644e9
Michella20 <andriafanomezantsoamichella@gmail.com>
2025-09-01 10:31:04 +0300

    Q13: ajout d'une méthode main() dans Femme pour tester l’amitié

--- Refactoring/src/iut/bad/Femme.java
+++ Refactoring/src/iut/bad/Femme.java
@@ -2,11 +2,17 @@ package iut.bad;
 
 public class Femme extends Humain {
 
-    public Femme() {
-        super();
-    }
-
     public Femme(String nom, String prenom, int age) {
         super(nom, prenom, age);
+    }
+
+    public static void main(String[] args) {
+        Homme homme = new Homme("Martin", "Jean", 25);
+        Femme femme = new Femme("Dupont", "Alice", 23);
+
+        homme.details();
+        femme.details();
+
+        femme.ami(homme);
     }
 }
